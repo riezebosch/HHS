7254497bf17c8927326eed2dc5bf391fea87f368
Manuel Riezebosch <mriezebosch@gmail.com>
2014-09-15 10:04:46 +0200

    Leeftijd berekenen met TDD

--- HHS/Class1.cs
+++ HHS/Class1.cs
@@ -8,5 +8,25 @@ namespace HHS
 {
     public class Class1
     {
+        public static int Bereken(DateTime geboortedatum)
+        {
+            var today = DateTime.Today;
+            var leeftijd =  DateTime.Today.Year - geboortedatum.Year;
+
+            if (geboortedatum.Month > today.Month)
+            {
+                leeftijd--;
+            }
+
+            if (geboortedatum.Month == today.Month && geboortedatum.Day > today.Day)
+            {
+                leeftijd--;
+            }
+
+            //if (DateTime.Today.Month < geboortedatum.Month )
+
+            return leeftijd;
+
+        }
     }
 }
