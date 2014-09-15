5397c3f0ca57279140bf75f1bb383118a4449038
Manuel Riezebosch <mriezebosch@gmail.com>
2014-09-15 13:49:24 +0200

    Linq Demo en oefening

---
+++ HHS.Tests/StringHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHS.Tests
+{
+    static class StringHelper
+    {
+        public static string RemoveAllVowels(this string input)
+        {
+            var sb = new StringBuilder();
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+            foreach (var item in input)
+            {
+                if (!vowels.Contains(item))
+                {
+                    sb.Append(item);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ HHS/Cup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHS
+{
+    public class Cup<TSDFLKJSDLFKJSLDF>
+    {
+        public TSDFLKJSDLFKJSLDF Beverage { get; set; }
+    }
+}
